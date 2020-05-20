ab3c83dd4f1c5e4a194bb174c28950bf244f072d
kenu <kenu.heo@gmail.com>
2020-05-20 17:45:24 +0900

    ajax cookie

---
+++ routes/cookie.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/server', function (req, res, next) {
+  var randomNumber = Math.random().toString();
+  randomNumber = randomNumber.substring(2, randomNumber.length);
+  const cookieName = 'cookie'+randomNumber.substring(0,3);
+  res.cookie(cookieName, randomNumber, { maxAge: 900000, httpOnly: true });
+  console.log('cookie created successfully');
+  res.json({ cookie: `cookie ${cookieName}` });
+});
+
+router.get('/get', function (req, res, next) {
+  var cookies = req.cookies;
+  console.log('cookie exists', cookies);
+  res.json({ cookies: cookies });
+});
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/cookie-test', function(req, res, next) {
+  res.render('cookie-test', { title: 'Express' });
+});
+
+router.get('/cookie-get', function(req, res, next) {
+  res.render('cookie-get', { title: 'Express' });
+});
+
+module.exports = router;
