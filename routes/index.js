78c89f2ddb769b23d6e1db41a2dcf800aefd34e1
kenu <kenu@okjsp.pe.kr>
2020-08-19 14:43:56 +0900

    title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cross Domain' });
 });
 
 router.get('/cookie-set', function(req, res, next) {
@@ -11,7 +11,7 @@ router.get('/cookie-set', function(req, res, next) {
 });
 
 router.get('/cookie-get', function(req, res, next) {
-  res.render('cookie-get', { title: 'Express' });
+  res.render('cookie-get', { title: 'Get Cookie' });
 });
 
 router.get('/login', function(req, res, next) {
